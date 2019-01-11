bf3b3a798816d3c6e1f893422b68a355dc125d81
Jeffrey Yang <jeffreyyang@Jeffreys-MacBook-Air.local>
2019-01-10 16:18:53 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <dankman message1="asasdfdf"></dankman>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import dankman from './components/ex.vue'
+export default {
+  name: 'app',
+  components: {
+    dankman,
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ex.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bonkman">
+        <h1>{{message1}}</h1>
+        <button @click="exMethod()">test</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "dankman",
+        props: {
+            message1: String
+        },
+        data: function() {
+            return {
+                message: "fromData"
+            }
+        },
+        methods: {
+            exMethod: function() {
+                this.message1 = "changed"
+            }
+        }
+
+       
+        
+    }
+</script>
+
