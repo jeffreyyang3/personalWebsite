d7f6d47ff36a02e1139b3527732573dfb5f52a8b
Jeffrey Yang <jeffreyyang@Jeffreys-MacBook-Air.local>
2019-01-10 16:29:20 -0800

    projdesc

---
+++ src/components/projDesc.vue
@@ -0,0 +1,20 @@
+// project description
+
+<template>
+    <div class="projectDesc">
+        <h2> {{ title }} </h2>
+        <h4> {{ technologies }}</h4>
+        <p> {{ description }} </p>
+        {{ source }}
+        
+
+
+
+
+
+
+
+
+
+    </div>
+</template>
