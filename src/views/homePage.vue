86e89dd774e914a99d1ed158eb8e0d7a0aad4229
Jeffrey Yang <jeffreyyang@eduroam-169-233-143-15.ucsc.edu>
2019-01-11 12:04:59 -0800

    sort to home

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -2,7 +2,12 @@
 
 <template>
     <div class="homePage">
-       xx
+       {{ bonkman }}
+       <ul v-for="bar in array" 
+       v-bind:style="{ margin: '45px', marginTop: '-30px'}"
+       v-bind:key="bar">
+		<div v-bind:style="divClasses(bar)"> </div>
+	    </ul>
     </div>
 </template>
 
@@ -10,6 +15,107 @@
     
     export default {
         name: "homePage",
+        data: function(){
+            return{
+                array: [],
+                upper: 0,
+            }
+        },
+        methods:{
+            insertionSort: function(){
+                setTimeout(function timeoutSort(array, i) {
+
+                    if (i < array.length) {
+                        
+                        this.arrayStep(array, i);
+                        
+                        setTimeout(timeoutSort.bind(this), 25, array, i + 1);
+                        
+                    }
+                    else{
+                        sortDone = true
+                    }
+                }.bind(this), 25, this.array, 0);
+
+                
+
+                return this.array;
+
+        },
+
+        createArray: function (arraySize) {
+            this.array = []
+            for(let i = 1; i < arraySize; i++){ 
+                this.array.push(i)
+            }
+            
+
+            this.Shuffle(this.array)
+            
+        },
+        arrayStep: function(arry,i){
+            arry.push()
+            var temp = arry[i];
+            var j = i - 1;
+            while (j >= 0 && arry[j] > temp) {
+                arry[j + 1] = arry[j];
+                j--;
+            }
+            arry[j + 1] = temp;
+    
+        },
+
+        Shuffle(o) {
+            for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+            return o;
+        },
+        
+        divClasses: function(value) {
+            return {
+           
+                width: '5px',
+                height: value * .05 + 'em',
+                display: 'block',
+                border: ".03em solid black",        
+                backgroundColor: this.rainbowArray[value - 1],
+                float: 'left',
+                marginTop: '0px',
+                backgroundColor: this.rainbowArray[value - 1]
+            
+
+            }
+            
+        },
+        createRainbowArray: function(array){
+            for (let i = 0; i < array.length; i++) {
+                let red   = this.sin_to_hex(i, 0 * Math.PI * 2/3, array.length); // 0   deg
+                let blue  = this.sin_to_hex(i, 1 * Math.PI * 2/3, array.length); // 120 deg
+                let green = this.sin_to_hex(i, 2 * Math.PI * 2/3, array.length); // 240 deg
+                array[i] = "#"+ red + green + blue;
+              }
+        },
+        sin_to_hex: function(i, phase, size) {
+            var sin = Math.sin(Math.PI / size * 2 * i + phase);
+            var int = Math.floor(sin * 127) + 128;
+            var hex = int.toString(16);
+          
+            return hex.length === 1 ? "0"+hex : hex;
+        }
+
+    },
+    mounted(){
+        this.createArray(100);
+        this.rainbowArray = new Array(this.array.length)
+        this.createRainbowArray(this.rainbowArray)
+        setTimeout(()=>{
+            this.insertionSort()
+        }, 200)
+       
+        
+    },
+    
+            
+        
 
     }
     
